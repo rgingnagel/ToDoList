9be93b6642e0a7f4631f4d38cb3e6656dd52c55d
Rens Gingnagel <rensgingnagel@gmail.com>
2017-11-24 13:14:44 +0100

    Initial Commit

---
+++ ToDoList/ToDoCell.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoCell.swift
+//  ToDoList
+//
+//  Created by Rens Gingnagel on 24/11/2017.
+//  Copyright © 2017 Rens Gingnagel. All rights reserved.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var isCompleteButton: UIButton!
+}
---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Rens Gingnagel on 23/11/2017.
+//  Copyright © 2017 Rens Gingnagel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
